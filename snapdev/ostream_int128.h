787dd628de2aefd96251081a4729692312eb25bd
AlexisWilke <alexis@m2osw.com>
2021-01-26 09:29:21 -0800

    Added support to convert 128 bit integers to string.

---
+++ snapdev/ostream_int128.h
@@ -0,0 +1,158 @@
+// Output __int128 or unsigned __int128 to an iostream
+// Copyright (c) 2021  Made to Order Software Corp.  All Rights Reserved
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#pragma once
+
+/** \file
+ * \brief Print large integers to any iostream.
+ *
+ * This function prints large integers (128 bits) to the specified iostream.
+ */
+
+// C++ lib
+//
+#include    <ostream>
+#include    <sstream>
+
+
+namespace snap
+{
+
+/** \brief Convert an __int128 to a string.
+ *
+ * This function converts an __int128 to a string.
+ *
+ * \param[in] x  An __int128 number.
+ *
+ * \return A string representing that __int128 number.
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+std::string to_string(__int128 x)
+{
+    char buf[64];
+
+    int idx(sizeof(buf) - 1);
+    buf[idx] = '\0';
+    unsigned __int128 y;
+    if(x < 0)
+    {
+        y = static_cast<unsigned __int128>(-x);
+    }
+    else
+    {
+        y = static_cast<unsigned __int128>(x);
+    }
+    while(y > 9)
+    {
+        --idx;
+        buf[idx] = y % 10 + '0';
+        y /= 10;
+    }
+    --idx;
+    buf[idx] = y + '0';
+    if(x < 0)
+    {
+        --idx;
+        buf[idx] = '-';
+    }
+
+    return std::string(buf + idx);
+}
+#pragma GCC diagnostic pop
+
+
+/** \brief Convert an unsigned __int128 to a string.
+ *
+ * This function converts an unsigned __int128 to a string.
+ *
+ * \param[in] x  An unsigned __int128 number.
+ *
+ * \return A string representing that unsigned __int128 number.
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+std::string to_string(unsigned __int128 x)
+{
+    char buf[64];
+
+    int idx(sizeof(buf) - 1);
+    buf[idx] = '\0';
+    while(x > 9)
+    {
+        --idx;
+        buf[idx] = x % 10 + '0';
+        x /= 10;
+    }
+    --idx;
+    buf[idx] = x + '0';
+
+    return std::string(buf + idx);
+}
+#pragma GCC diagnostic pop
+
+}
+
+
+
+//namespace snap -- no namespace for such definitions, it works better that way
+
+
+/** \brief Output an __int128 number.
+ *
+ * \warning
+ * This implementation does not offer proper formatting.
+ * It will first generate a string then output that string
+ * which means the result is not what you'd expect if you
+ * used formatting such as std::hex and std::setw().
+ *
+ * \param[in] os  The output stream.
+ * \param[in] x  The value to be output in \p os.
+ *
+ * \return A reference to \p os.
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+std::ostream & operator << (std::ostream & os, __int128 x)
+{
+    return os << snap::to_string(x);
+}
+#pragma GCC diagnostic pop
+
+
+/** \brief Output an unsigned __int128 number.
+ *
+ * \warning
+ * This implementation does not offer proper formatting.
+ * It will first generate a string then output that string
+ * which means the result is not what you'd expect if you
+ * used formatting such as std::hex and std::setw().
+ *
+ * \param[in] os  The output stream.
+ * \param[in] x  The value to be output in \p os.
+ *
+ * \return A reference to \p os.
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+std::ostream & operator << (std::ostream & os, unsigned __int128 x)
+{
+    return os << snap::to_string(x);
+}
+#pragma GCC diagnostic pop
+
+
+// vim: ts=4 sw=4 et
---
+++ tests/int128.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2021  Made to Order Software Corp.  All Rights Reserved
+ *
+ * https://snapwebsites.org/
+ * contact@m2osw.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/** \file
+ * \brief Verify the various __int128 output support.
+ *
+ * This file implements tests to verify that the support to print out
+ * large numbers works as expected.
+ */
+
+// self
+//
+#include "main.h"
+
+
+
+// snapdev lib
+//
+#include "snapdev/ostream_int128.h"
+
+
+#pragma GCC diagnostic ignored "-Wpedantic"
+
+
+CATCH_TEST_CASE("ostream_int128", "[ostream][int128]")
+{
+    CATCH_START_SECTION("small numbers (-10 to +10)")
+    {
+        for(int i(-10); i <= 10; ++i)
+        {
+            __int128 l(i);
+            std::stringstream ss;
+            ss << l;
+            CATCH_REQUIRE(ss.str() == std::to_string(i));
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("positive numbers")
+    {
+        for(int i(0); i < 1000; ++i)
+        {
+            std::int32_t v((rand() & 0x7FFFFFFF));
+            __int128 l(v);
+            std::stringstream ss;
+            ss << l;
+            CATCH_REQUIRE(ss.str() == std::to_string(v));
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("negative numbers")
+    {
+        for(int i(0); i < 1000; ++i)
+        {
+            std::int32_t v(-(rand() & 0x7FFFFFFF));
+            __int128 l(v);
+            std::stringstream ss;
+            ss << l;
+            CATCH_REQUIRE(ss.str() == std::to_string(v));
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("most negative number")
+    {
+        __int128 l(1);
+        l <<= 127;
+        std::stringstream ss;
+        ss << l;
+        CATCH_REQUIRE(ss.str() == "-170141183460469231731687303715884105728");
+    }
+    CATCH_END_SECTION()
+}
+
+
+// vim: ts=4 sw=4 et
