9c9d207f447b9ef767e915d9fc815cab96df98a4
AlexisWilke <alexis@m2osw.com>
2022-01-09 22:43:01 -0800

    Added a SAFE_ASSERT() function.
    The assert() "function" is notoriously unsafe because it uses a macro. Now a
    day, there are features in each compiler to verify for side effects, but the
    truth is that if you call a function that gets completely removed at runtime,
    you may end up with a side effect issue between Debug and Release.
    
    Added a test to verify the SAFE_ASSERT().

---
+++ snapdev/safe_assert.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2022  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/snapdev
+// contact@m2osw.com
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#pragma once
+
+// self
+//
+#include    <snapdev/not_used.h>
+
+
+// C++ lib
+//
+#include    <iostream>
+
+
+
+namespace snap
+{
+
+/** \brief A debug/non-debug assert() which can include side effects.
+ *
+ * This function is safe to use without any special protection. The
+ * expression to verify will be optimized out, except for any part which
+ * may have side effects.
+ *
+ * \param[in] test_result  The value to be tested, if true, the assert passes.
+ * \param[in] ... args  Arguments to pass to std::cerr for a user defined
+ * message.
+ */
+template<typename ... Args>
+void SAFE_ASSERT(bool test_result, Args && ... args)
+{
+#ifdef _DEBUG
+    if(!test_result)
+    {
+        (std::cerr << ... << args);
+        std::cerr << std::endl;
+        std::terminate();
+    }
+#else
+    NOT_USED(test_result, args...);
+#endif
+}
+
+
+
+}
+// snap namespace
+// vim: ts=4 sw=4 et
---
+++ tests/catch_assert.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2022  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/snapdev
+// contact@m2osw.com
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+/** \file
+ * \brief Verify that the SAFE_ASSERT function works.
+ *
+ * This file verifies we can use the SAFE_ASSERT function as expected.
+ */
+
+// self
+//
+#include    <snapdev/safe_assert.h>
+
+#include    "catch_main.h"
+
+
+// last include
+//
+#include    <snapdev/poison.h>
+
+
+
+
+CATCH_TEST_CASE("safe_assert", "[foo]")
+{
+    CATCH_START_SECTION("safe_assert: make sure assert can pass")
+    {
+        constexpr bool valid(true);
+        snap::SAFE_ASSERT(valid, "this works");
+    }
+    CATCH_END_SECTION()
+
+#ifdef NDEBUG
+    CATCH_START_SECTION("safe_assert: test invalid assert in Release mode")
+    {
+        constexpr bool invalid(false);
+        snap::SAFE_ASSERT(invalid, "this \"passes\"", ", but really it's not compiled in in Release mode");
+    }
+    CATCH_END_SECTION()
+#endif
+}
+
+
+
+// vim: ts=4 sw=4 et
