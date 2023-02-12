f7e89af34e78370a51d943366a066b687148e257
AlexisWilke <alexis@m2osw.com>
2023-02-12 10:24:01 -0800

    Added unsorted_remove_duplicates() and tests.

--- snapdev/remove_duplicates.h
+++ snapdev/remove_duplicates.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2022  Made to Order Software Corp.  All Rights Reserved
+// Copyright (c) 2011-2023  Made to Order Software Corp.  All Rights Reserved
 //
 // https://snapwebsites.org/project/snapdev
 // contact@m2osw.com
@@ -26,10 +26,14 @@
  * Some containers, such as a vector, allow for duplicates. This function
  * implements an algorithm to remove duplicates.
  *
- * \important
- * If your vector is already sorted, you may want to consider using the
- * erase/unique directly instead of calling this function which will
- * call sort() again.
+ * We offer several solutions to the problem of removing duplicates:
+ *
+ * \li The default algorithm which sorts the values first; this is the
+ * \em normal algorithm for such a feature.
+ * \li Consider using a container that automatically sorts such as
+ * `std::set<T>` and `std::map<T,bool>`.
+ * \li There are unsorted versions of containers that you may be able
+ * to use for the purpose.
  */
 
 // C++
@@ -42,31 +46,129 @@ namespace snapdev
 {
 
 
-/** \brief Remove duplicates in the specified container.
+/** \brief Remove duplicates in the unsorted container.
  *
- * This function goes through the elements of your container after sorting
- * them and eliminates duplicates.
+ * This function goes through the elements of your container and first
+ * sorts and then eliminates duplicates.
  *
- * This function works with vectors.
+ * This function works well with vectors.
  *
  * \note
  * Maps and sets are already sorted and already do not include duplicates.
- * In other words, this function is useless for these containers.
+ * In other words, this function is useless for such containers.
  *
  * \warning
  * On return, the elements in the container are sorted.
  *
- * \tparam ContainerT  The type of string.
+ * \tparam ContainerT  The type of container to de-dup.
  * \param[in,out] container  The container where duplicates get removed.
  *
  * \return The reference to the input container (NOT A COPY).
+ *
+ * \sa sorted_remove_duplicates()
+ * \sa unsorted_remove_duplicates()
  */
 template<typename ContainerT>
-ContainerT & remove_duplicates(ContainerT & container)
+ContainerT & sort_and_remove_duplicates(ContainerT & container)
 {
     std::sort(container.begin(), container.end());
 
     container.erase(std::unique(
+                          container.begin()
+                        , container.end())
+                    , container.end());
+
+    return container;
+}
+
+
+/** \brief Remove duplicates in the sorted container.
+ *
+ * This function goes through the elements of your container and eliminates
+ * duplicates. The input must be sorted for the algorithm to work properly.
+ *
+ * \note
+ * Maps and sets are already sorted and already do not include duplicates.
+ * In other words, this function is useless for such containers.
+ *
+ * \tparam ContainerT  The type of container to de-dup.
+ * \param[in,out] container  The container where duplicates get removed.
+ *
+ * \return The reference to the input container (NOT A COPY).
+ *
+ * \sa sort_and_remove_duplicates()
+ */
+template<typename ContainerT>
+ContainerT & sorted_remove_duplicates(ContainerT & container)
+{
+    container.erase(std::unique(
+                          container.begin()
+                        , container.end())
+                    , container.end());
+
+    return container;
+}
+
+
+/** \brief Function to replace duplicate objects.
+ *
+ * This function goes through the items of a container and overwrite
+ * entries as duplicates are found.
+ *
+ * \warning
+ * Like std::unique(), this function does not itself remove anything.
+ * You must make sure to use the std::erase() to remove the last few
+ * items. This is done by the unsorted_remove_duplicates() function.
+ *
+ * \rparam ForwardIterator  The type of iterator from your container.
+ * \param[in] first  Forward iterator to the container's first element.
+ * \param[in] end  Forward iterator to the container's last element + 1.
+ *
+ * \return The new end of your container.
+ *
+ * \sa https://stackoverflow.com/questions/49863158/c-remove-duplicate-values-from-a-vector-without-sorting
+ * \sa unsorted_remove_duplicates()
+ */
+template<typename ForwardIterator>
+ForwardIterator remove_duplicates(ForwardIterator first, ForwardIterator end)
+{
+    auto new_end(first);
+    for(auto current(first); current != end; ++current)
+    {
+        if(std::find(first, new_end, *current) == new_end)
+        {
+            if(new_end != current)
+            {
+                *new_end = *current;
+            }
+            ++new_end;
+        }
+    }
+
+    return new_end;
+}
+
+
+/** \brief Remove duplicates without sorting the container.
+ *
+ * This function goes through the elements of your container and eliminates
+ * duplicates. The input does not have to be sorted and the returned function
+ * does not change the order.
+ *
+ * The algorithm is O(n^2). If sorting is okay, then use the
+ * sort_and_remove_duplicates() directly.
+ *
+ * \tparam ContainerT  The type of container to de-dup.
+ * \param[in,out] container  The container where duplicates get removed.
+ *
+ * \return The reference to the input container (NOT A COPY).
+ *
+ * \sa remove_duplicates()
+ */
+template<typename ContainerT>
+ContainerT & unsorted_remove_duplicates(ContainerT & container)
+{
+    container.erase(remove_duplicates(
                           container.begin()
                         , container.end())
                     , container.end());
---
+++ tests/catch_remove_duplicates.cpp
@@ -0,0 +1,365 @@
+// Copyright (c) 2018-2023  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/snapdev
+// contact@m2osw.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/** \file
+ * \brief Verify that the different remove duplicate algorithms work.
+ *
+ * This file implements tests for the different remove duplicate functions.
+ */
+
+// self
+//
+#include    <snapdev/remove_duplicates.h>
+
+#include    "catch_main.h"
+
+
+// C++
+//
+#include    <deque>
+#include    <list>
+
+
+// last include
+//
+#include    <snapdev/poison.h>
+
+
+namespace
+{
+
+
+
+
+
+}
+// no name namespace
+
+
+
+CATCH_TEST_CASE("sort_and_remove_duplicates", "[remove_duplicates]")
+{
+    CATCH_START_SECTION("sort_and_remove_duplicates: empty container")
+    {
+        std::vector<std::string> empty;
+        std::vector<std::string> *output = &snapdev::sort_and_remove_duplicates(empty);
+        CATCH_REQUIRE(&empty == output);
+        CATCH_REQUIRE(empty.empty());
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("sort_and_remove_duplicates: container without duplicates")
+    {
+        for(int count(0); count < 10; ++count)
+        {
+            std::vector<std::string> container;
+            std::size_t size(rand() % 100 + 50);
+            while(container.size() < size)
+            {
+                std::string const s(SNAP_CATCH2_NAMESPACE::random_string(0, rand() % 25));
+                if(std::find(container.begin(), container.end(), s) == container.end())
+                {
+                    container.push_back(s);
+                }
+            }
+            std::vector<std::string> copy(container);
+            std::vector<std::string> *output = &snapdev::sort_and_remove_duplicates(container);
+            CATCH_REQUIRE(&container == output);
+            CATCH_REQUIRE(container.size() == copy.size());
+
+            std::sort(copy.begin(), copy.end());
+            CATCH_REQUIRE(container == copy);
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("sort_and_remove_duplicates: container with duplicates")
+    {
+        for(int count(0); count < 10; ++count)
+        {
+            std::vector<std::string> container;
+
+            // we want to be in control of the duplication and know exactly what
+            // we are going to get back out so we need to create a container
+            // without duplicates first then add duplicates in a second loop
+            //
+            std::size_t size(rand() % 100 + 50);
+            while(container.size() < size)
+            {
+                std::string const s(SNAP_CATCH2_NAMESPACE::random_string(0, rand() % 25));
+                if(std::find(container.begin(), container.end(), s) == container.end())
+                {
+                    container.push_back(s);
+                }
+            }
+            std::vector<std::string> copy(container);
+            std::size_t dups(rand() % 50 + 25);
+            for(std::size_t i(0); i < dups; ++i)
+            {
+                int const d(rand() % container.size());
+                int const p(rand() % (container.size() + 1));
+                container.insert(container.begin() + p, container[d]);
+            }
+            std::vector<std::string> *output = &snapdev::sort_and_remove_duplicates(container);
+            CATCH_REQUIRE(&container == output);
+            CATCH_REQUIRE(container.size() == copy.size());
+
+            std::sort(copy.begin(), copy.end());
+            CATCH_REQUIRE(container == copy);
+        }
+    }
+    CATCH_END_SECTION()
+}
+
+
+CATCH_TEST_CASE("sorted_remove_duplicates", "[remove_duplicates]")
+{
+    CATCH_START_SECTION("sorted_remove_duplicates: empty container")
+    {
+        std::vector<std::string> empty;
+        std::vector<std::string> *output = &snapdev::sorted_remove_duplicates(empty);
+        CATCH_REQUIRE(&empty == output);
+        CATCH_REQUIRE(empty.empty());
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("sorted_remove_duplicates: container without duplicates")
+    {
+        for(int count(0); count < 10; ++count)
+        {
+            std::vector<std::string> container;
+            std::size_t size(rand() % 100 + 50);
+            while(container.size() < size)
+            {
+                std::string const s(SNAP_CATCH2_NAMESPACE::random_string(0, rand() % 25));
+                if(std::find(container.begin(), container.end(), s) == container.end())
+                {
+                    container.push_back(s);
+                }
+            }
+            std::sort(container.begin(), container.end());
+            std::vector<std::string> copy(container);
+            std::vector<std::string> *output = &snapdev::sorted_remove_duplicates(container);
+            CATCH_REQUIRE(&container == output);
+            CATCH_REQUIRE(container.size() == copy.size());
+            CATCH_REQUIRE(container == copy);
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("sorted_remove_duplicates: container with duplicates")
+    {
+        for(int count(0); count < 10; ++count)
+        {
+            std::vector<std::string> container;
+
+            // we want to be in control of the duplication and know exactly what
+            // we are going to get back out so we need to create a container
+            // without duplicates first then add duplicates in a second loop
+            //
+            std::size_t size(rand() % 100 + 50);
+            while(container.size() < size)
+            {
+                std::string const s(SNAP_CATCH2_NAMESPACE::random_string(0, rand() % 25));
+                if(std::find(container.begin(), container.end(), s) == container.end())
+                {
+                    container.push_back(s);
+                }
+            }
+            std::vector<std::string> copy(container);
+            std::size_t dups(rand() % 50 + 25);
+            for(std::size_t i(0); i < dups; ++i)
+            {
+                int const d(rand() % container.size());
+                int const p(rand() % (container.size() + 1));
+                container.insert(container.begin() + p, container[d]);
+            }
+            std::sort(container.begin(), container.end());
+            std::vector<std::string> *output = &snapdev::sorted_remove_duplicates(container);
+            CATCH_REQUIRE(&container == output);
+            CATCH_REQUIRE(container.size() == copy.size());
+
+            std::sort(copy.begin(), copy.end());
+            CATCH_REQUIRE(container == copy);
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("sorted_remove_duplicates: container with one item duplicated")
+    {
+        for(int count(0); count < 10; ++count)
+        {
+            std::list<std::string> container;
+
+            // we want to be in control of the duplication and know exactly what
+            // we are going to get back out so we need to create a container
+            // without duplicates first then add duplicates in a second loop
+            //
+            std::string const s(SNAP_CATCH2_NAMESPACE::random_string(0, rand() % 25));
+            std::size_t size(rand() % 100 + 50);
+            while(container.size() < size)
+            {
+                container.push_back(s);
+            }
+            std::list<std::string> *output = &snapdev::sorted_remove_duplicates(container);
+            CATCH_REQUIRE(&container == output);
+            CATCH_REQUIRE(container.size() == 1);
+            CATCH_REQUIRE(container.front() == s);
+        }
+    }
+    CATCH_END_SECTION()
+}
+
+
+CATCH_TEST_CASE("unsorted_remove_duplicates", "[remove_duplicates]")
+{
+    CATCH_START_SECTION("unsorted_remove_duplicates: empty container")
+    {
+        std::vector<std::string> empty;
+        std::vector<std::string> *output = &snapdev::unsorted_remove_duplicates(empty);
+        CATCH_REQUIRE(&empty == output);
+        CATCH_REQUIRE(empty.empty());
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("unsorted_remove_duplicates: container without duplicates")
+    {
+        std::vector<std::string> container;
+        std::size_t size(rand() % 100 + 50);
+        while(container.size() < size)
+        {
+            std::string const s(SNAP_CATCH2_NAMESPACE::random_string(0, rand() % 25));
+            if(std::find(container.begin(), container.end(), s) == container.end())
+            {
+                container.push_back(s);
+            }
+        }
+        std::vector<std::string> copy(container);
+        std::vector<std::string> *output = &snapdev::unsorted_remove_duplicates(container);
+        CATCH_REQUIRE(&container == output);
+        CATCH_REQUIRE(container.size() == copy.size());
+        CATCH_REQUIRE(container == copy);
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("unsorted_remove_duplicates: container with duplicates (vector)")
+    {
+        std::vector<std::string> container;
+
+        // we want to be in control of the duplication and know exactly what
+        // we are going to get back out so we need to create a container
+        // without duplicates first then add duplicates in a second loop
+        //
+        std::size_t size(rand() % 100 + 50);
+        while(container.size() < size)
+        {
+            std::string const s(SNAP_CATCH2_NAMESPACE::random_string(0, rand() % 25));
+            if(std::find(container.begin(), container.end(), s) == container.end())
+            {
+                container.push_back(s);
+            }
+        }
+        std::vector<std::string> copy(container);
+        std::size_t dups(rand() % 50 + 25);
+        for(std::size_t i(0); i < dups; ++i)
+        {
+            int const d(rand() % container.size());
+
+            // since items do not get sorted, we need p > d for this test
+            // to work properly
+            //
+            int const p(d + rand() % (container.size() - d) + 1);
+
+            container.insert(container.begin() + p, container[d]);
+        }
+        std::vector<std::string> *output = &snapdev::unsorted_remove_duplicates(container);
+        CATCH_REQUIRE(&container == output);
+        CATCH_REQUIRE(container.size() == copy.size());
+        CATCH_REQUIRE(container == copy);
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("unsorted_remove_duplicates: container with duplicates (list)")
+    {
+        std::list<std::string> container;
+
+        // we want to be in control of the duplication and know exactly what
+        // we are going to get back out so we need to create a container
+        // without duplicates first then add duplicates in a second loop
+        //
+        std::size_t size(rand() % 100 + 50);
+        while(container.size() < size)
+        {
+            std::string const s(SNAP_CATCH2_NAMESPACE::random_string(0, rand() % 25));
+            if(std::find(container.begin(), container.end(), s) == container.end())
+            {
+                container.push_back(s);
+            }
+        }
+        std::list<std::string> copy(container);
+        std::size_t dups(rand() % 50 + 25);
+        for(std::size_t i(0); i < dups; ++i)
+        {
+            int const d(rand() % container.size());
+
+            // since items do not get sorted, we need p > d for this test
+            // to work properly
+            //
+            int const p(d + rand() % (container.size() - d) + 1);
+
+            auto d_it(container.begin());
+            std::advance(d_it, d);
+            auto p_it(container.begin());
+            std::advance(p_it, p);
+            container.insert(p_it, *d_it);
+        }
+        std::list<std::string> *output = &snapdev::unsorted_remove_duplicates(container);
+        CATCH_REQUIRE(&container == output);
+        CATCH_REQUIRE(container.size() == copy.size());
+        CATCH_REQUIRE(container == copy);
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("unsorted_remove_duplicates: container with one item duplicated")
+    {
+        for(int count(0); count < 10; ++count)
+        {
+            std::deque<std::string> container;
+
+            // we want to be in control of the duplication and know exactly what
+            // we are going to get back out so we need to create a container
+            // without duplicates first then add duplicates in a second loop
+            //
+            std::string const s(SNAP_CATCH2_NAMESPACE::random_string(0, rand() % 25));
+            std::size_t size(rand() % 100 + 50);
+            while(container.size() < size)
+            {
+                container.push_back(s);
+            }
+            std::deque<std::string> *output = &snapdev::sorted_remove_duplicates(container);
+            CATCH_REQUIRE(&container == output);
+            CATCH_REQUIRE(container.size() == 1);
+            CATCH_REQUIRE(container.front() == s);
+        }
+    }
+    CATCH_END_SECTION()
+}
+
+
+
+// vim: ts=4 sw=4 et
