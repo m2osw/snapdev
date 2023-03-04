551faad57fe6202db072d71d36a629e2ba35b5a4
AlexisWilke <alexis@m2osw.com>
2023-03-04 09:29:11 -0800

    Added the stringize.h header to avoid boost.

---
+++ snapdev/stringize.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2023  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/snapdev
+// contact@m2osw.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#pragma once
+
+/** \file
+ * \brief Transform a value in a string at preprocessor time.
+ *
+ * It is often useful to transform a value to a string at compile time so
+ * it can integrated in a string. This preprocessor macro can be used for
+ * that purpose.
+ *
+ * \todo
+ * Eventually offer versions that work with all preprocessors--i.e. see
+ * the corresponding boost file:
+ *
+ * \code
+ *     /usr/include/boost/preprocessor/stringize.hpp
+ * \endcode
+ */
+
+#define SNAPDEV_STRINGIZE(text) SNAPDEV_STRINGIZE_I(text)
+#define SNAPDEV_STRINGIZE_I(text) #text
+
+// vim: ts=4 sw=4 et
---
+++ tests/catch_stringize.cpp
@@ -0,0 +1,58 @@
+// Copyright (c) 2023  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/snapdev
+// contact@m2osw.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/** \file
+ * \brief Verify that SNAPDEV_STRINGIZE() works as expected.
+ *
+ * This file implements tests to verify the stringize macro.
+ */
+
+// self
+//
+#include    <snapdev/stringize.h>
+
+#include    "catch_main.h"
+
+
+// last include
+//
+#include    <snapdev/poison.h>
+
+
+
+CATCH_TEST_CASE("stringize", "[string]")
+{
+    CATCH_START_SECTION("stringize: build year")
+    {
+        std::string copyright("Copyright (c) 2023-"
+            SNAPDEV_STRINGIZE(UTC_BUILD_YEAR)
+            " by Made to Order Software Corporation, All Rights Reserved");
+
+        std::stringstream ss;
+        ss << "Copyright (c) 2023-";
+        ss << UTC_BUILD_YEAR;
+        ss << " by Made to Order Software Corporation, All Rights Reserved";
+
+        CATCH_REQUIRE(ss.str() == copyright);
+    }
+    CATCH_END_SECTION()
+}
+
+
+
+// vim: ts=4 sw=4 et
