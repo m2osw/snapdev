58e0b8f8a90cf63c422dd5667b4d0c9c29d77890
AlexisWilke <alexis@m2osw.com>
2024-05-17 21:51:36 -0700

    Added NOT_REACHED_IN_TEST() for added protection.
    Many of my tests cannot reach certain parts of the code, or at least, I
    suspect that it's not reachable. If it does get there, though, we want
    to get an error when running the final coverage test. This function
    does nothing when not running those tests. However, if running those
    tests and it gets called, then it abort()'s the test, which the catch2
    library captures. As a result, we can be sure that it really wasn't
    called. The function cames with an example on how to use it.

--- snapdev/not_reached.h
+++ snapdev/not_reached.h
@@ -45,6 +45,8 @@
 namespace snapdev
 {
 
+
+
 [[noreturn]] [[deprecated]] inline void NOTREACHED()
 {
     std::cerr << "NOT_REACHED called, process will abort." << std::endl;
@@ -59,6 +61,7 @@ namespace snapdev
     abort();
 }
 
+
 [[noreturn]] inline void NOT_REACHED()
 {
     std::cerr << "NOT_REACHED called, process will abort." << std::endl;
@@ -72,6 +75,74 @@ namespace snapdev
 
     abort();
 }
+
+
+/** \brief Check whether code is unexpectendly reached in a test.
+ *
+ * Some tests verify that code is being reached by generating all the
+ * possible errors. This way we can eventually get full coverage unit
+ * tests.
+ *
+ * At times, there is just no way to generate certain errors. For
+ * example, when getting the current clock, the function may return
+ * an error. However, in all likelihood, it will work 100% of the
+ * time. So code checking such errors is generally put between
+ * macros so they don't participate in coverage tests. However,
+ * if that error just prints a message, it is likely to be transparent
+ * to the unit test, even if covered.
+ *
+ * So to make sure that it is indeed not covered, we call this function.
+ *
+ * Here is an example:
+ *
+ * \code
+ *     if(!f_connection->send_message(lock_message))
+ *     {
+ *         // LCOV_EXCL_START
+ *         f_state = state_t::CLUCK_STATE_FAILED;
+ *         set_reason(reason_t::CLUCK_REASON_TRANSMISSION_ERROR);
+ *         snapdev::NOT_REACHED_IN_TEST();
+ *         return false;
+ *         // LCOV_EXCL_STOP
+ *     }
+ * \endcode
+ *
+ * If somehow with time the send_message() ends up returning false, this
+ * code will execute and end up calling this NOT_REACHED_IN_TEST() function
+ * which calls abort() after writing an error message when running a coverage
+ * test.
+ *
+ * \note
+ * The function does absolutely nothing when not building using the sanitizer
+ * (which we expect is used when testing only).
+ *
+ * \note
+ * The function is not marked as "no return" since it does return when not
+ * running the coverage test. Actually, it does absolutely nothing in all
+ * other cases.
+ *
+ * \note
+ * If your ignored code always ends by raising an exception, then there is
+ * no need to call this function. Your test should not expect that exception
+ * so you'll know if the test fails in that unexpected way.
+ */
+inline void NOT_REACHED_IN_TEST()
+{
+#ifdef __SANITIZE_ADDRESS__
+    std::cerr << "NOT_REACHED_TEST called, process will abort." << std::endl;
+
+    std::cerr << "Stack trace:" << std::endl;
+    libexcept::stack_trace_t trace(libexcept::collect_stack_trace_with_line_numbers());
+    for(auto l : trace)
+    {
+        std::cerr << "  " << l << std::endl;
+    }
+
+   abort();
+#endif
+}
+
+
 
 } // namespace snapdev
 // vim: ts=4 sw=4 et
