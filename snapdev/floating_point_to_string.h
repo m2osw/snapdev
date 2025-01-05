efb6c465a4da01e619d74b94a3482a9f16f101ff
AlexisWilke <alexis@m2osw.com>
2025-01-04 22:05:54 -0800

    Adding a "float to string" function.
    The function was taken from as2js (the floating_point.h). That way we can
    convert floating points to a string in other places too. At the moment
    it would be useful in prinbee. This function converts NaN and Infinity
    as such strings instead of failing or returning an invalid floating
    point representation.

---
+++ snapdev/floating_point_to_string.h
@@ -0,0 +1,113 @@
+// Copyright (c) 2019-2024  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/snapdev
+// contact@m2osw.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#pragma once
+
+/** \file
+ * \brief Create a list of files using glob().
+ *
+ * This template allows you to insert filenames from the output of a glob()
+ * call to an STL container.
+ */
+
+// snapdev
+//
+//#include    "snapdev/pathinfo.h"
+//#include    "snapdev/raii_generic_deleter.h"
+
+
+// C++
+//
+#include    <cmath>
+//#include    <memory>
+#include    <string>
+
+
+// C
+//
+//#include    <limits.h>
+//#include    <stdlib.h>
+
+
+
+namespace snapdev
+{
+
+
+
+template<
+    typename F,
+    class CharT,
+    class Traits = std::char_traits<CharT>,
+    class Allocator = std::allocator<CharT>>
+std::basic_string<CharT, Traits, Allocator> floating_point_to_string(F value, bool keep_period = true)
+{
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+    if(std::isnan(value))
+    {
+        return std::basic_string<CharT, Traits, Allocator>("NaN");
+    }
+    else if(value == 0.0)
+    {
+        if(keep_period)
+        {
+            return std::basic_string<CharT, Traits, Allocator>("0.0");
+        }
+        return std::basic_string<CharT, Traits, Allocator>("0");
+    }
+    else if(std::isinf(value))
+    {
+        if(std::signbit(value))
+        {
+            return std::basic_string<CharT, Traits, Allocator>("-Infinity");
+        }
+        else
+        {
+            return std::basic_string<CharT, Traits, Allocator>("Infinity");
+        }
+    }
+    else
+    {
+        std::basic_string<CharT, Traits, Allocator> str(std::to_string(value));
+        if(str.find('.') != str.npos)
+        {
+            while(str.back() == '0')
+            {
+                str.pop_back();
+            }
+            if(str.back() == '.')
+            {
+                if(keep_period)
+                {
+                    str += '0';
+                }
+                else
+                {
+                    str.pop_back();
+                }
+            }
+        }
+        return str;
+    }
+#pragma GCC diagnostic pop
+}
+
+
+
+} // namespace snapdev
+// vim: ts=4 sw=4 et
---
+++ tests/catch_floating_point_to_string.cpp
@@ -0,0 +1,140 @@
+// Copyright (c) 2021-2024  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/snapdev
+// contact@m2osw.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/** \file
+ * \brief Verify the floating point to string convertions.
+ *
+ * This file implements tests for the floating point to string.
+ */
+
+// self
+//
+#include    <snapdev/floating_point_to_string.h>
+
+#include    "catch_main.h"
+
+
+
+// C++ lib
+//
+//#include    <iomanip>
+//#include    <set>
+
+
+// last include
+//
+#include    <snapdev/poison.h>
+
+
+
+namespace
+{
+
+
+
+
+
+} // no name namespace
+
+
+
+CATCH_TEST_CASE("floating_point_to_string", "[float][string]")
+{
+    CATCH_START_SECTION("floating_point_to_string: verify conversion of floating points to string")
+    {
+        struct conversion_t
+        {
+            double      f_floating_point = 0.0;
+            bool        f_keep_period = true;
+            std::string f_string = std::string();
+        };
+
+        conversion_t const conversions[] =
+        {
+            {
+                .f_floating_point = 0.0,
+                .f_keep_period = true,
+                .f_string = "0.0",
+            },
+            {
+                .f_floating_point = 0.0,
+                .f_keep_period = false,
+                .f_string = "0",
+            },
+            {
+                .f_floating_point = 1.0,
+                .f_keep_period = true,
+                .f_string = "1.0",
+            },
+            {
+                .f_floating_point = 1.0,
+                .f_keep_period = false,
+                .f_string = "1",
+            },
+            {
+                .f_floating_point = -1.0,
+                .f_keep_period = true,
+                .f_string = "-1.0",
+            },
+            {
+                .f_floating_point = -1.0,
+                .f_keep_period = false,
+                .f_string = "-1",
+            },
+            {
+                .f_floating_point = std::numeric_limits<double>::infinity(),
+                .f_keep_period = true,
+                .f_string = "Infinity",
+            },
+            {
+                .f_floating_point = std::numeric_limits<double>::infinity(),
+                .f_keep_period = false,
+                .f_string = "Infinity",
+            },
+            {
+                .f_floating_point = -std::numeric_limits<double>::infinity(),
+                .f_keep_period = true,
+                .f_string = "-Infinity",
+            },
+            {
+                .f_floating_point = -std::numeric_limits<double>::infinity(),
+                .f_keep_period = false,
+                .f_string = "-Infinity",
+            },
+        };
+
+        for(auto const & c : conversions)
+        {
+            if(c.f_keep_period)
+            {
+                // try with the default
+                //
+                std::string const default_keep_period(snapdev::floating_point_to_string<double, char>(c.f_floating_point));
+                CATCH_REQUIRE(c.f_string == default_keep_period);
+            }
+
+            std::string const result(snapdev::floating_point_to_string<double, char>(c.f_floating_point, c.f_keep_period));
+            CATCH_REQUIRE(c.f_string == result);
+        }
+    }
+    CATCH_END_SECTION()
+}
+
+
+
+// vim: ts=4 sw=4 et
